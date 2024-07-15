69b7d401772ef6ce25684d8c32a8996e0abe26e2
Noah765 <noland62007@gmail.com>
2024-07-15 20:37:38 +0200

    monitor setup

--- hosts/primary/configuration.nix
+++ hosts/primary/configuration.nix
@@ -6,7 +6,13 @@
   nvidia.enable = true;
   stylix.enable = true;
   sddm.enable = true;
-  hyprland.enable = true;
+  hyprland = {
+    enable = true;
+    settings.monitor = [
+      "DP-1, 1920x1080@144, 0x0, 1"
+      "HDMI-A-1, 1920x1080@75, 1920x0, 1"
+    ];
+  };
   anyrun.enable = true;
 
   os.environment.systemPackages = with pkgs; [
