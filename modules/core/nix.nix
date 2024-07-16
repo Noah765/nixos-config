c072423d3edfee55ae459bc620b671e58cc67130
Noah765 <noland62007@gmail.com>
2024-07-16 19:04:01 +0200

    nix settings, nix-dram

--- modules/core/nix.nix
+++ modules/core/nix.nix
@@ -10,18 +10,39 @@ let
   cfg = config.nix;
 in
 {
+  inputs.nix-dram.url = "github:dramforever/nix-dram";
+
   options.nix.enable = mkEnableOption "nix";
 
   config.os = mkIf cfg.enable {
     nixpkgs.config.allowUnfree = true;
 
-    nix.package = pkgs.nix.overrideAttrs (old: {
-      patches = old.patches or [ ] ++ [
-        (pkgs.fetchurl {
-          url = "https://raw.githubusercontent.com/Noah765/combined-manager/main/evaluable-flake.patch";
-          hash = "sha256-UZ5hXI1w1mOEe0Bp5rSfeB4jfnwxnNEXJWir4dQGyyo=";
-        })
-      ];
-    });
+    nix = {
+      package = (inputs.nix-dram.packages.${pkgs.system}.default).overrideAttrs (old: {
+        patches = old.patches or [ ] ++ [
+          (pkgs.fetchurl {
+            url = "https://raw.githubusercontent.com/Noah765/combined-manager/main/nix-patches/2.22.1/evaluable-flake.patch";
+            hash = "sha256-/VoR8Ygm4bHPVqNz7PkKMoptDSqV666R0xza/YBfKEE=";
+          })
+        ];
+      });
+
+      gc = {
+        automatic = true;
+        dates = "monthly";
+        options = "--delete-older-than 30d";
+      };
+
+      settings = {
+        experimental-features = [
+          "nix-command"
+          "flakes"
+        ];
+
+        auto-optimise-store = true;
+
+        default-flake = "github:NixOS/nixpkgs/nixos-unstable";
+      };
+    };
   };
 }
