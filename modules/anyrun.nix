3c38620eaa56da0a18fe1be0b1cb1002ad09c83b
Noah765 <noland62007@gmail.com>
2024-07-16 10:50:05 +0200

    styling anyrun

--- modules/anyrun.nix
+++ modules/anyrun.nix
@@ -25,16 +25,52 @@ in
         rink
         shell
       ];
-      y.absolute = 15;
+      y.absolute = 30;
       width.fraction = 0.3;
       hidePluginInfo = true;
       closeOnClick = true;
     };
 
     extraCss = ''
-      window {
+      #window {
         background: transparent;
       }
+
+      list#main {
+        margin-top: 0.5rem; 
+        border: 1px solid @theme_bg_color;
+      }
+
+      /*
+      #match.activatable {
+          border-radius: 16px;
+          padding: 0.3rem 0.9rem;
+          margin-top: 0.01rem;
+      }
+      #match.activatable:first-child {
+          margin-top: 0.0rem;
+      }
+      #match.activatable:last-child {
+          margin-bottom: 0.6rem;
+      }
+
+      #entry {
+          border: 1px solid #0b0f10;
+          border-radius: 16px;
+          margin: 0.5rem;
+          padding: 0.3rem 1rem;
+      }
+
+      list > #plugin {
+          margin: 0 0.3rem;
+      }
+      list > #plugin:first-child {
+          margin-top: 0.3rem;
+      }
+      list > #plugin:last-child {
+          margin-bottom: 0.3rem;
+      }
+      */
     '';
   };
 }
