c6e7c143e2fbbc22b15219e797b84221f049a8db
Noah765 <noland62007@gmail.com>
2024-07-17 10:16:55 +0200

    walker config

--- modules/walker.nix
+++ modules/walker.nix
@@ -25,81 +25,24 @@ in
       enable = true;
       runAsService = true;
 
+      # TODO Clipboard module
       config = {
         placeholder = "Search...";
-        ignore_mouse = false;
-        ssh_host_file = "";
-        disable_up_history = false;
-        enable_typeahead = false;
+        enable_typeahead = true; # TODO It isn't showing up
         show_initial_entries = true;
         fullscreen = false;
-        scrollbar_policy = "automatic";
-        websearch.engines = [ "google" ];
-        hyprland.context_aware_history = false;
-        applications = {
-          enable_cache = false;
-          ignore_actions = false;
-        };
-        activation_mode = {
-          disabled = false;
-          use_f_keys = false;
-          use_alt = false;
-        };
-        search = {
-          delay = 0;
-          hide_icons = false;
-          margin_spinner = 10;
-          hide_spinner = false;
-        };
-        runner.excludes = [ "rm" ];
-        clipboard = {
-          max_entries = 10;
-          image_height = 300;
-        };
-        align = {
-          ignore_exlusive = true;
-          width = 400;
-          horizontal = "center";
-          vertical = "start";
-          anchors = {
-            top = false;
-            left = false;
-            bottom = false;
-            right = false;
-          };
-          margins = {
-            top = 20;
-            bottom = 0;
-            end = 0;
-            start = 0;
-          };
-        };
+        hyprland.context_aware_history = false; # TODO
+        align.width = 400;
         list = {
           height = 300;
           margin_top = 10;
-          always_show = true;
-          hide_sub = false;
-          max_entries = 50;
         };
-        orientation = "vertical";
-        icons = {
-          theme = "";
-          hide = false;
-          size = 28;
-          image_height = 200;
-        };
+        icons.size = 28;
         modules = [
           { name = "applications"; }
+          { name = "websearch"; }
           {
-            name = "websearch";
-            prefix = "?";
-          }
-          {
-            name = "finder";
-            prefix = "/";
-          }
-          {
-            name = "emojis";
+            name = "hyprland";
             prefix = ":";
           }
         ];
