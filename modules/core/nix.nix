32ca55cdb02432440b1ba95749ca5d3b345ba9e7
Noah765 <noland62007@gmail.com>
2024-07-21 15:32:54 +0200

    Move away from requiring Home Manager in core modules

--- modules/core/nix.nix
+++ modules/core/nix.nix
@@ -19,55 +19,47 @@ in {
     };
   };
 
-  hmImports = [inputs.nix-index-database.hmModules.nix-index];
+  osImports = [inputs.nix-index-database.nixosModules.nix-index];
 
   options.nix.enable = mkEnableOption "nix";
 
-  config = mkIf cfg.enable {
-    os = mkIf cfg.enable {
-      nixpkgs.config.allowUnfree = true;
-
-      nix = {
-        package = (inputs.nix-dram.packages.${pkgs.system}.default).overrideAttrs (old: {
-          patches =
-            old.patches
-            or []
-            ++ [
-              (pkgs.fetchpatch {
-                url = "https://raw.githubusercontent.com/Noah765/combined-manager/main/nix-patches/2.22.1/evaluable-flake.patch";
-                hash = "sha256-/VoR8Ygm4bHPVqNz7PkKMoptDSqV666R0xza/YBfKEE=";
-              })
-            ];
-        });
-
-        settings = {
-          experimental-features = [
-            "nix-command"
-            "flakes"
+  config.os = mkIf cfg.enable {
+    nix = {
+      package = inputs.nix-dram.packages.${pkgs.system}.default.overrideAttrs (old: {
+        patches =
+          old.patches
+          or []
+          ++ [
+            (pkgs.fetchurl {
+              url = "https://raw.githubusercontent.com/Noah765/combined-manager/main/nix-patches/2.22.1/evaluable-flake.patch";
+              hash = "sha256-/VoR8Ygm4bHPVqNz7PkKMoptDSqV666R0xza/YBfKEE=";
+            })
           ];
-          auto-optimise-store = true;
-          default-flake = "github:NixOS/nixpkgs/nixos-unstable";
-        };
-      };
+      });
 
-      programs.nh = {
+      settings = {
+        experimental-features = [
+          "nix-command"
+          "flakes"
+        ];
+        auto-optimise-store = true;
+        default-flake = "github:NixOS/nixpkgs/nixos-unstable";
+      };
+    };
+
+    nixpkgs.config.allowUnfree = true;
+
+    programs.nh = {
+      enable = true;
+      clean = {
         enable = true;
-        clean = {
-          enable = true;
-          extraArgs = "-k 5 -K 7d";
-        };
-        flake = "/etc/nixos";
+        extraArgs = "-k 5 -K 7d";
       };
+      flake = "/etc/nixos";
     };
 
-    hm = {
-      home.packages = with pkgs; [
-        alejandra
-        nix-output-monitor
-        deadnix
-      ];
+    programs.nix-index-database.comma.enable = true;
 
-      programs.nix-index-database.comma.enable = true;
-    };
+    environment.systemPackages = with pkgs; [alejandra nix-output-monitor deadnix];
   };
 }
