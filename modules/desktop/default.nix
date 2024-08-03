22c1bdc1706239bf815fbad537016117ba4a2c3b
Noah765 <noland62007@gmail.com>
2024-08-03 14:33:39 +0200

    waybar

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -11,14 +11,16 @@ in {
     ./sddm.nix
     ./hyprland.nix
     ./walker.nix
+    ./waybar.nix
   ];
 
-  options.desktop.enable = mkEnableOption "the default window manager, display manager, app runner, etc";
+  options.desktop.enable = mkEnableOption "the default window manager, display manager, app runner, status bar, etc";
 
   config.desktop = mkIf cfg.enable {
     stylix.enable = mkDefault true;
     sddm.enable = mkDefault true;
     hyprland.enable = mkDefault true;
     walker.enable = mkDefault true;
+    waybar.enable = mkDefault true;
   };
 }
---
+++ modules/desktop/waybar.nix
@@ -0,0 +1,35 @@
+{
+  lib,
+  config,
+  ...
+}:
+with lib; let
+  cfg = config.desktop.waybar;
+in {
+  options.desktop.waybar.enable = mkEnableOption "Waybar";
+
+  config = mkIf cfg.enable {
+    assertions = [
+      {
+        assertion = config.desktop.stylix.enable;
+        message = "The waybar module is dependent on the stylix module.";
+      }
+    ];
+
+    hm.programs.waybar = {
+      enable = true;
+      # TODO systemd.enable
+
+      settings = {
+        modules-left = ["image" "hyprland/workspaces"];
+        modules-center = ["clock"];
+
+        image.path = "/home/noah/Downloads/nix-snowflake.svg";
+        "hyprland/workspaces" = {
+          persistent-workspaces."*" = [1 2 3 4 5 6 7 8 9 10];
+          move-to-monitor = true;
+        };
+      };
+    };
+  };
+}
