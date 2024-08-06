db09c66ace65364771bff8428b5625e51d97ca57
Noah765 <noland62007@gmail.com>
2024-08-06 18:52:40 +0200

    patch waybar to temporarily fix it

--- modules/desktop/waybar.nix
+++ modules/desktop/waybar.nix
@@ -1,5 +1,6 @@
 {
   lib,
+  pkgs,
   config,
   ...
 }:
@@ -14,6 +15,34 @@ in {
         assertion = config.desktop.stylix.enable;
         message = "The waybar module is dependent on the stylix module.";
       }
+    ];
+
+    # TODO Temporarily fixes https://github.com/Alexays/Waybar/issues/3302
+    os.nixpkgs.overlays = [
+      (final: prev: {
+        waybar = prev.waybar.overrideAttrs (old: {
+          patches =
+            old.patches
+            or []
+            ++ [
+              (pkgs.writeText "waybar-patch" ''
+                diff --git a/src/util/backlight_backend.cpp b/src/util/backlight_backend.cpp
+                index bb102cd9..c2e9710c 100644
+                --- a/src/util/backlight_backend.cpp
+                +++ b/src/util/backlight_backend.cpp
+                @@ -153,7 +153,7 @@ BacklightBackend::BacklightBackend(std::chrono::milliseconds interval,
+                   // Connect to the login interface
+                   login_proxy_ = Gio::DBus::Proxy::create_for_bus_sync(
+                       Gio::DBus::BusType::BUS_TYPE_SYSTEM, "org.freedesktop.login1",
+                -      "/org/freedesktop/login1/session/self", "org.freedesktop.login1.Session");
+                +      "/org/freedesktop/login1/session/auto", "org.freedesktop.login1.Session");
+
+                   udev_thread_ = [this] {
+                     std::unique_ptr<udev, UdevDeleter> udev{udev_new()};
+              '')
+            ];
+        });
+      })
     ];
 
     hm.programs.waybar = {
