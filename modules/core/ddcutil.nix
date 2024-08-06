2196ba8e7de86e594aa1d60079c4f39317153f49
Noah765 <noland62007@gmail.com>
2024-08-06 18:53:28 +0200

    ddcutil

--- modules/core/ddcutil.nix
+++ modules/core/ddcutil.nix
@@ -11,13 +11,40 @@ in {
   options.core.ddcutil.enable = mkEnableOption "ddcutil for controlling displays";
 
   config = mkIf cfg.enable {
+    # TODO Substitute with module definition?
     os.hardware.i2c.enable = true;
     core.user.groups = ["i2c"];
 
-    hm.home.packages = [pkgs.ddcutil];
-    os.boot = {
-      extraModulePackages = [osConfig.boot.kernelPackages.ddcci-driver];
-      kernelModules = ["ddcci_backlight"]; # TODO Needed?
+    os = {
+      environment.systemPackages = [pkgs.ddcutil];
+      boot.extraModulePackages = [osConfig.boot.kernelPackages.ddcci-driver];
+
+      services.udev.extraRules = ''
+        SUBSYSTEM=="i2c-dev", ACTION=="add",\
+          ATTR{name}=="NVIDIA i2c adapter*",\
+          TAG+="ddcci",\
+          TAG+="systemd",\
+          ENV{SYSTEMD_WANTS}+="ddcci@$kernel.service"
+      '';
+
+      systemd.services."ddcci@" = {
+        scriptArgs = "%i";
+        script = ''
+          echo Trying to attach ddcci to $1
+          i=0
+          id=$(echo $1 | cut -d "-" -f 2)
+          counter=5
+          while [ $counter -gt 0 ]; do
+            if ${pkgs.ddcutil}/bin/ddcutil getvcp 10 -b $id; then
+              echo ddcci 0x37 > /sys/bus/i2c/devices/$1/new_device
+              break
+            fi
+            sleep 1
+            counter=$((counter - 1))
+          done
+        '';
+        serviceConfig.Type = "oneshot";
+      };
     };
   };
 }
