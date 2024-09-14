8fe5c23bf73be8a3dbc2601e64a72bf66c43df50
Noah765 <noland62007@gmail.com>
2024-09-14 14:11:56 +0200

    explicitly disable PipeWire

--- modules/core/pulseaudio.nix
+++ modules/core/pulseaudio.nix
@@ -8,8 +8,13 @@ with lib; let
 in {
   options.core.pulseaudio.enable = mkEnableOption "PulseAudio";
 
-  config.os.hardware.pulseaudio = mkIf cfg.enable {
-    enable = true;
-    support32Bit = true;
+  # TODO Switch to PipeWire when moving away from waybar
+  config.os = mkIf cfg.enable {
+    hardware.pulseaudio = {
+      enable = true;
+      support32Bit = true;
+    };
+
+    services.pipewire.enable = false;
   };
 }
