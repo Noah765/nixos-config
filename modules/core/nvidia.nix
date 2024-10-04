c1023a090d56e9e1e698cec21524c28889086921
Noah765 <noland62007@gmail.com>
2024-10-04 17:50:32 +0200

    cleaned up nvidia module

--- modules/core/nvidia.nix
+++ modules/core/nvidia.nix
@@ -9,8 +9,6 @@ in {
   options.core.nvidia.enable = mkEnableOption "Nvidia drivers";
 
   config.os = mkIf cfg.enable {
-    hardware.graphics.enable = true;
-
     services.xserver.videoDrivers = ["nvidia"]; # This does not enable xserver, the name is historical
 
     hardware.nvidia = {
