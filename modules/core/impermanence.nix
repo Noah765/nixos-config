d5807277cf5765f0bb922d3a1c42dc8810b16d38
Noah765 <noland62007@gmail.com>
2024-10-24 11:56:13 +0200

    updated the impermanence module

--- modules/core/impermanence.nix
+++ modules/core/impermanence.nix
@@ -16,7 +16,7 @@ in {
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    impermanence.url = "github:tmarkov/impermanence"; # TODO: Change to nix-community when they fixed https://github.com/nix-community/impermanence/issues/154
+    impermanence.url = "github:nix-community/impermanence";
   };
 
   osImports = [
@@ -116,30 +116,36 @@ in {
         };
       };
 
-      boot.initrd.postDeviceCommands = lib.mkAfter ''
-        mkdir /btrfs_tmp
-        mount /dev/root_vg/root /btrfs_tmp
-        if [[ -e /btrfs_tmp/root ]]; then
-          mkdir -p /btrfs_tmp/old_roots
-          timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
-          mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
-        fi
+      boot.initrd.systemd.services.impermanence = {
+        description = "Creates a new root subvolume and gradually deletes old ones.";
+        wants = ["dev-root_vg-root.device"];
+        after = ["dev-root_vg-root.device"];
+        serviceConfig.Type = "oneshot";
+        script = ''
+          mkdir /btrfs_tmp
+          mount /dev/root_vg/root /btrfs_tmp
+          if [[ -e /btrfs_tmp/root ]]; then
+            mkdir -p /btrfs_tmp/old_roots
+            timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
+            mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
+          fi
 
-        delete_subvolume_recursively() {
-          IFS=$'\n'
-          for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-            delete_subvolume_recursively "/btrfs_tmp/$i"
+          delete_subvolume_recursively() {
+            IFS=$'\n'
+            for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
+              delete_subvolume_recursively "/btrfs_tmp/$i"
+            done
+            btrfs subvolume delete "$1"
+          }
+
+          for i in $(find /btrfs_tmp/old_roots/ -mindepth 1 -maxdepth 1 -mtime +30); do
+            delete_subvolume_recursively "$i"
           done
-          btrfs subvolume delete "$1"
-        }
 
-        for i in $(find /btrfs_tmp/old_roots/ -mindepth 1 -maxdepth 1 -mtime +30); do
-          delete_subvolume_recursively "$i"
-        done
-
-        btrfs subvolume create /btrfs_tmp/root
-        umount /btrfs_tmp
-      '';
+          btrfs subvolume create /btrfs_tmp/root
+          umount /btrfs_tmp
+        '';
+      };
 
       fileSystems."/persist".neededForBoot = true;
       environment.persistence."/persist/system" = {
