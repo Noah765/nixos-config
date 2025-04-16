3986ffb5bf5624d2d0f9a802c81760922621b056
Noah765 <noland62007@gmail.com>
2025-04-16 10:19:28 +0200

    temporary nushell config

--- modules/cli/nushell.nix
+++ modules/cli/nushell.nix
@@ -14,6 +14,27 @@ in {
 
     hm.programs.nushell = {
       enable = true;
+      settings = {
+        history.file_format = "sqlite";
+        # TODO history.isolation
+        show_banner = false;
+        edit_mode = "vi";
+        # TODO cursor_shape
+        completions.algorithm = "fuzzy";
+        # TODO completions.case_sensitive, completions.partial, completions.external.{max_results, completer}
+        # TODO use_kitty_protocol, shell_integration.osc9_9
+        # TODO display_errors.exit_code
+        # TODO footer_mode, table.trim
+
+        # TODO table.abbreviated_row_count = 15;
+        # TODO explore, history.file_format
+        filesize.metric = true;
+        # color_config, footer_mode
+        # TODO env.editor is wrong
+        # TODO render_right_prompt_on_last_line
+      };
     };
+
+    hm.programs.nixvim.plugins.lsp.servers.nushell.enable = true;
   };
 }
