e19c4d786f4d327347f5efa39546376421988162
Noah765 <noah.landgraf@gmx.de>
2025-09-21 08:30:27 +0200

    cli/editor/lsp: set up keymaps

--- modules/cli/editor/lsp.nix
+++ modules/cli/editor/lsp.nix
@@ -7,5 +7,69 @@
 
   options.cli.editor.lsp.enable = lib.mkEnableOption "lsp";
 
-  config.cli.editor.settings.plugins.lspconfig.enable = lib.mkIf config.cli.editor.lsp.enable true;
+  config = lib.mkIf config.cli.editor.lsp.enable {
+    dependencies = ["cli.editor.telescope"];
+
+    cli.editor.settings.plugins = {
+      lspconfig.enable = true;
+      which-key.settings.spec = lib.singleton (config.hm.lib.nixvim.utils.listToUnkeyedAttrs ["<leader>l"] // {group = "LSP";});
+    };
+
+    cli.editor.settings.lsp.keymaps = [
+      {
+        key = "<leader>j";
+        mode = "n";
+        action = config.hm.lib.nixvim.mkRaw "function() vim.diagnostic.jump({ count = 1, float = true }) end";
+        options.desc = "Next diagnostic";
+      }
+      {
+        key = "<leader>k";
+        mode = "n";
+        action = config.hm.lib.nixvim.mkRaw "function() vim.diagnostic.jump({ count = -1, float = true }) end";
+        options.desc = "Previous diagnostic";
+      }
+      {
+        key = "<leader>q";
+        mode = "n";
+        action = config.hm.lib.nixvim.mkRaw "require('telescope.builtin').quickfix";
+        options.desc = "Quickfix";
+      }
+      {
+        key = "<leader>lD";
+        mode = "n";
+        action = config.hm.lib.nixvim.mkRaw "require('telescope.builtin').diagnostics";
+        options.desc = "Diagnostics";
+      }
+      {
+        key = "<leader>lq";
+        mode = "n";
+        action = config.hm.lib.nixvim.mkRaw "require('telescope.builtin').loclist";
+        options.desc = "Quickfixs";
+      }
+      {
+        key = "<leader>lt";
+        mode = "n";
+        action = config.hm.lib.nixvim.mkRaw "require('telescope.builtin').lsp_type_definitions";
+        options.desc = "Type definitions";
+      }
+      {
+        key = "<leader>ld";
+        mode = "n";
+        action = config.hm.lib.nixvim.mkRaw "require('telescope.builtin').lsp_definitions";
+        options.desc = "Definitions";
+      }
+      {
+        key = "<leader>li";
+        mode = "n";
+        action = config.hm.lib.nixvim.mkRaw "require('telescope.builtin').lsp_implementations";
+        options.desc = "Implementations";
+      }
+      {
+        key = "<leader>lr";
+        mode = "n";
+        action = config.hm.lib.nixvim.mkRaw "require('telescope.builtin').lsp_references";
+        options.desc = "References";
+      }
+    ];
+  };
 }
