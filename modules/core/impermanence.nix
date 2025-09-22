90087fe6bb11c3c7db366d7ceffd2839a19f6449
Noah765 <noah.landgraf@gmx.de>
2025-09-22 08:33:53 +0200

    core/impermanence: fix initrd script execution

--- modules/core/impermanence.nix
+++ modules/core/impermanence.nix
@@ -109,38 +109,30 @@
           };
         };
 
-        boot.initrd.systemd.services.impermanence = {
-          description = "Creates a new root subvolume and gradually deletes old ones.";
-          wantedBy = ["initrd.target"];
-          after = ["initrd-root-device.target"];
-          before = ["sysroot.mount"];
-          unitConfig.DefaultDependencies = "no";
-          serviceConfig.Type = "oneshot";
-          script = ''
-            mkdir /btrfs_tmp
-            mount /dev/root_vg/root /btrfs_tmp
-            if [[ -e /btrfs_tmp/root ]]; then
-              mkdir -p /btrfs_tmp/old_roots
-              timestamp=$(date --date="@$(stat -c %X /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
-              mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
-            fi
+        boot.initrd.postResumeCommands = lib.mkAfter ''
+          mkdir /btrfs_tmp
+          mount /dev/root_vg/root /btrfs_tmp
+          if [[ -e /btrfs_tmp/root ]]; then
+            mkdir -p /btrfs_tmp/old_roots
+            timestamp=$(date --date="@$(stat -c %X /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
+            mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
+          fi
 
-            delete_subvolume_recursively() {
-              IFS=$'\n'
-              for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-                delete_subvolume_recursively "/btrfs_tmp/$i"
-              done
-              btrfs subvolume delete "$1"
-            }
-
-            for i in $(find /btrfs_tmp/old_roots/ -mindepth 1 -maxdepth 1 -mtime +30); do
-              delete_subvolume_recursively "$i"
+          delete_subvolume_recursively() {
+            IFS=$'\n'
+            for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
+              delete_subvolume_recursively "/btrfs_tmp/$i"
             done
+            btrfs subvolume delete "$1"
+          }
 
-            btrfs subvolume create /btrfs_tmp/root
-            umount /btrfs_tmp
-          '';
-        };
+          for i in $(find /btrfs_tmp/old_roots/ -mindepth 1 -maxdepth 1 -mtime +30); do
+            delete_subvolume_recursively "$i"
+          done
+
+          btrfs subvolume create /btrfs_tmp/root
+          umount /btrfs_tmp
+        '';
 
         fileSystems."/persist".neededForBoot = true;
         environment.persistence."/persist/system" = {
