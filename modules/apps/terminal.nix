e62c15c19d024c227cdd156564ba07e7c1df1aae
Noah765 <noah.landgraf@gmx.de>
2025-10-29 20:30:22 +0100

    apps/terminal: remove unused keybindings

--- modules/apps/terminal.nix
+++ modules/apps/terminal.nix
@@ -24,31 +24,9 @@
       };
 
       keybindings = {
-        "alt+r" = "launch --cwd=current --location=vsplit";
-        "alt+s" = "launch --cwd=current --location=hsplit";
         "alt+t" = "launch --cwd=current --type=tab";
-
         "alt+h" = "previous_tab";
         "alt+l" = "next_tab";
-        "alt+1" = "goto_tab 1";
-        "alt+2" = "goto_tab 2";
-        "alt+3" = "goto_tab 3";
-        "alt+4" = "goto_tab 4";
-        "alt+5" = "goto_tab 5";
-        "alt+6" = "goto_tab 6";
-        "alt+7" = "goto_tab 7";
-        "alt+8" = "goto_tab 8";
-        "alt+9" = "goto_tab 9";
-        "alt+0" = "goto_tab 0";
-
-        "alt+shift+h" = "move_window left";
-        "alt+shift+j" = "move_window down";
-        "alt+shift+k" = "move_window up";
-        "alt+shift+l" = "move_window right";
-
-        "alt+ctrl+shift+h" = "move_tab_backward";
-        "alt+ctrl+shift+l" = "move_tab_forward";
-
         "alt+q" = "close_window";
       };
     };
