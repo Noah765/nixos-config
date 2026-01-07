ff07139c9789e0e9695ef36f7e36c63cebde2bf1
Noah765 <noah.landgraf@gmx.de>
2026-01-07 22:15:34 +0100

    dev/basic: disable direnv timeout warning

--- modules/dev/basic.nix
+++ modules/dev/basic.nix
@@ -8,11 +8,14 @@
 
   config = lib.mkIf config.dev.basic.enable {
     hm.home.packages = [pkgs.nix];
+
     hm.programs.direnv = {
       enable = true;
+      config.global.warn_timeout = 0;
       nix-direnv.enable = true;
       silent = true;
     };
+
     # TODO automatically collect garbage using direnv prune
     core.impermanence.hm.directories = [".local/share/direnv/allow"];
   };
