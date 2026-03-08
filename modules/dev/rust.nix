0f17a16d4ed4ed70bd9e769a6152a47e68bebf61
Noah765 <noah.landgraf@gmx.de>
2026-03-08 18:01:09 +0100

    dev/rust: configure rust-analyzer

--- modules/dev/rust.nix
+++ modules/dev/rust.nix
@@ -9,8 +9,24 @@
     config = lib.mkIf config.dev.rust.enable {
       core.impermanence.hm.directories = [".cargo"];
 
-      cli.editor.packages = [pkgs.rust-analyzer pkgs.rustc];
-      cli.editor.languages.rust.language-servers = ["rust-analyzer" "codebook"];
+      cli.editor = {
+        packages = with pkgs; [clippy rust-analyzer rustc];
+
+        languages.rust.language-servers = ["rust-analyzer" "codebook"];
+
+        languageServers.rust-analyzer.config = {
+          assist.preferSelf = true;
+          check.command = "clippy";
+          completion.hideDeprecated = true;
+          completion.termSearch.enable = true;
+          diagnostics.styleLints.enable = true;
+          gotoImplementations.filterAdjacentDerives = true;
+          hover.show.traitAssocItems = 5;
+          imports.preferPrelude = true;
+          references.excludeImports = true;
+          references.excludeTests = true;
+        };
+      };
     };
   };
 }
