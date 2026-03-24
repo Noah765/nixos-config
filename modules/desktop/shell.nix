d01b71587c1a8774e1176403aedb188e463b5abe
Noah765 <noah.landgraf@gmx.de>
2026-03-24 19:16:53 +0100

    desktop/shell: enable blur behind bar

--- modules/desktop/shell.nix
+++ modules/desktop/shell.nix
@@ -10,15 +10,22 @@
   }: {
     options.desktop.shell.enable = lib.mkEnableOption "the desktop shell";
 
-    config.desktop.hyprland.settings.exec-once = lib.mkIf config.desktop.shell.enable (lib.singleton (lib.join " " [
-      (lib.getExe inputs.shell.packages.${pkgs.stdenv.system}.default)
-      "--wallpaper-background=${config.theme.wallpaper.background}"
-      "--wallpaper-middle-ground=${config.theme.wallpaper.middleGround}"
-      "--wallpaper-foreground=${config.theme.wallpaper.foreground}"
-      "--background-color=${config.theme.colors.base00}"
-      "--text-color=${config.theme.colors.base05}"
-      "--primary-color=${config.theme.colors.base0B}"
-      "--bar-opacity=${lib.toString config.theme.windowOpacity}"
-    ]));
+    config.desktop.hyprland.settings = lib.mkIf config.desktop.shell.enable {
+      exec-once = lib.singleton (lib.join " " [
+        (lib.getExe inputs.shell.packages.${pkgs.stdenv.system}.default)
+        "--wallpaper-background=${config.theme.wallpaper.background}"
+        "--wallpaper-middle-ground=${config.theme.wallpaper.middleGround}"
+        "--wallpaper-foreground=${config.theme.wallpaper.foreground}"
+        "--background-color=${config.theme.colors.base00}"
+        "--text-color=${config.theme.colors.base05}"
+        "--primary-color=${config.theme.colors.base0B}"
+        "--bar-opacity=${lib.toString config.theme.windowOpacity}"
+      ]);
+
+      layerrule = lib.mkIf (config.theme.windowOpacity != 1) [
+        "match:namespace shell-bar, blur on"
+        "match:namespace shell-bar, ignore_alpha 0"
+      ];
+    };
   };
 }
