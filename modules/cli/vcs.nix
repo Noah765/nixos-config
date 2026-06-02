cfa725214572646f756bbea4d2df35b0c8506442
Noah765 <noah.landgraf@gmx.de>
2026-06-02 11:34:48 +0200

    cli/vcs: move to wrapper

--- modules/cli/vcs.nix
+++ modules/cli/vcs.nix
@@ -1,83 +1,20 @@
-{lib, ...}: {
-  nixos = {
-    pkgs,
-    config,
-    ...
-  }: {
+{
+  lib,
+  wlib,
+  ...
+}: {
+  nixos = {config, ...}: {
     options.cli.vcs = {
       enable = lib.mkEnableOption "Jujutsu, Jujutsu UI, Git and the GitHub CLI";
-      jj.enable = lib.mkEnableOption "Jujutsu" // {default = true;};
       jjui.enable = lib.mkEnableOption "Jujutsu UI" // {default = true;};
       git.enable = lib.mkEnableOption "Git" // {default = true;};
       gh.enable = lib.mkEnableOption "the GitHub CLI" // {default = true;};
     };
 
     config = lib.mkIf config.cli.vcs.enable {
+      wrappers.vcs.enable = true;
+
       hm.programs = {
-        jujutsu.enable = config.cli.vcs.jj.enable;
-
-        jujutsu.settings = {
-          user.name = "Noah765";
-          user.email = "noah.landgraf@gmx.de";
-
-          ui = {
-            default-command = "log";
-            diff-formatter = "${lib.getExe pkgs.difftastic} --color always --sort-paths $left $right";
-            movement.edit = true;
-            log-word-wrap = true;
-          };
-
-          templates.draft_commit_description = "builtin_draft_commit_description_with_diff";
-
-          # TODO diff editor and merge tool
-
-          fix.tools.treefmt.command = ["treefmt" "--quiet" "--stdin" "$path"];
-          fix.tools.treefmt.patterns = ["**"];
-
-          remotes.origin.auto-track-bookmarks = "*";
-          remotes.upstream.auto-track-bookmarks = "main|master";
-
-          fsmonitor.backend = "watchman";
-
-          aliases = lib.mapAttrs (name: script: ["util" "exec" "--" (pkgs.writers.writeNu "jj-${name}" script)]) {
-            push = ''
-              def main [revision: string = '@'] {
-                jj bookmark move main -t $revision
-                jj git push
-              }
-            '';
-
-            gh = ''
-              def main [name: string --private (-p)] {
-                ${lib.getExe pkgs.gh} repo create $name (if $private { '--private' } else { '--public' })
-                jj git clone $'https://github.com/Noah765/($name)'
-                cd $name
-                jj describe -m init
-                jj bookmark create main
-              }
-            '';
-
-            fork = ''
-              def main [owner: string repo: string] {
-                ${lib.getExe pkgs.gh} repo fork $'($owner)/($repo)' --clone=false --default-branch-only
-                main clone $repo --owner $owner
-              }
-
-              def "main clone" [repo: string --owner: string] {
-                jj git clone $'https://github.com/Noah765/($repo)'
-                cd $repo
-                let owner = if $owner != null { $owner } else { ${lib.getExe pkgs.gh} repo view $repo --json parent --jq .parent.owner.login }
-                jj git remote add upstream $'https://github.com/($owner)/($repo)'
-                jj config set --repo git.fetch '["origin", "upstream"]'
-                jj git fetch
-              }
-            '';
-          };
-        };
-
-        difftastic.enable = true;
-        difftastic.git.enable = true;
-
         jjui.enable = config.cli.vcs.jjui.enable;
         jjui.settings.preview.show_at_start = true;
 
@@ -91,10 +28,81 @@
         gh.enable = config.cli.vcs.gh.enable;
       };
 
-      hm.home.packages = lib.mkIf config.cli.vcs.jj.enable [pkgs.watchman];
-
-      core.impermanence.hm.directories = lib.mkIf config.cli.vcs.jj.enable [".config/jj/repos"];
+      core.impermanence.hm.directories = [".config/jj/repos"];
       core.impermanence.hm.files = lib.mkIf config.cli.vcs.gh.enable [".config/gh/hosts.yml"];
+    };
+  };
+
+  flake.wrappers.vcs = {pkgs, ...}: {
+    imports = [wlib.wrapperModules.jujutsu];
+
+    runtimePkgs = [pkgs.watchman];
+
+    settings = {
+      user.name = "Noah765";
+      user.email = "noah.landgraf@gmx.de";
+
+      ui = {
+        default-command = "log";
+        diff-formatter = [(lib.getExe pkgs.difftastic) "--color=always" "$left" "$right"];
+        movement.edit = true;
+        log-word-wrap = true;
+        show-cryptographic-signatures = true;
+      };
+
+      merge-tools.inline.program = lib.getExe pkgs.difftastic;
+      merge-tools.inline.diff-args = ["--display=inline" "--color=always" "$left" "$right"];
+
+      templates.draft_commit_description = "builtin_draft_commit_description_with_diff";
+      template-aliases."format_timestamp(timestamp)" = "timestamp.ago()";
+      template-aliases."format_short_signature(signature)" = "signature.name()";
+
+      revsets.bookmark-advance-to = "heads(::@ & mutable() & ~description(exact:\"\") & (~empty() | merges()))";
+
+      fix.tools.treefmt.command = ["treefmt" "--quiet" "--stdin" "$path"];
+      fix.tools.treefmt.patterns = ["**"];
+
+      remotes.origin.auto-track-bookmarks = "*";
+      remotes.upstream.auto-track-bookmarks = "main|master";
+
+      git.executable-path = lib.getExe pkgs.git;
+
+      fsmonitor.backend = "watchman";
+
+      aliases = lib.mapAttrs (name: script: ["util" "exec" "--" (pkgs.writers.writeNu "jj-${name}" script)]) {
+        push = ''
+          def main [revision: string = '@-'] {
+            jj bookmark move main --to $revision
+            jj git push
+          }
+        '';
+
+        gh = ''
+          def main [name: string --private (-p)] {
+            ${lib.getExe pkgs.gh} repo create $name (if $private { '--private' } else { '--public' })
+            jj git clone $'https://github.com/Noah765/($name)'
+            cd $name
+            jj describe --message init
+            jj bookmark create main
+          }
+        '';
+
+        fork = ''
+          def main [owner: string repo: string] {
+            ${lib.getExe pkgs.gh} repo fork $'($owner)/($repo)' --clone=false --default-branch-only
+            main clone $repo --owner $owner
+          }
+
+          def "main clone" [repo: string --owner: string] {
+            jj git clone $'https://github.com/Noah765/($repo)'
+            cd $repo
+            let owner = if $owner != null { $owner } else { ${lib.getExe pkgs.gh} repo view $repo --json parent --jq .parent.owner.login }
+            jj git remote add upstream $'https://github.com/($owner)/($repo)'
+            jj config set --repo git.fetch '["origin", "upstream"]'
+            jj git fetch
+          }
+        '';
+      };
     };
   };
 }
