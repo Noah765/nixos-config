151f12826692417cd4b0bc4d6f47fa1fd6fda389
Noah765 <noah.landgraf@gmx.de>
2026-06-03 18:55:43 +0200

    apps/terminal: use key value generator

--- modules/apps/terminal.nix
+++ modules/apps/terminal.nix
@@ -31,47 +31,57 @@
     flags."--config-file" = config.constructFiles.config.path;
 
     constructFiles.config.relPath = "${config.binName}-config";
-    constructFiles.config.content = ''
-      font-family = JetBrainsMono Nerd Font Mono
-      font-family = DejaVu Sans Mono
-      font-family = Unifont
-      font-size = 10.5
-      mouse-scroll-multiplier = 0.75
-      window-padding-x = 0
-      window-padding-y = 0
-      window-padding-balance = true
-      confirm-close-surface = false
-      app-notifications = no-clipboard-copy
+    constructFiles.config.content =
+      lib.generators.toKeyValue {
+        mkKeyValue = lib.generators.mkKeyValueDefault {} " = ";
+        listsAsDuplicateKeys = true;
+      } {
+        font-family = [
+          "JetBrainsMono Nerd Font Mono"
+          "DejaVu Sans Mono"
+          "Unifont"
+        ];
+        font-size = 10.5;
+        mouse-scroll-multiplier = 0.75;
+        window-padding-x = 0;
+        window-padding-y = 0;
+        window-padding-balance = true;
+        confirm-close-surface = false;
+        app-notifications = "no-clipboard-copy";
 
-      background = #2d353b
-      foreground = #d3c6aa
-      cursor-color = #d3c6aa
-      selection-background = #475258
-      selection-foreground = #d3c6aa
+        background = "#2d353b";
+        foreground = "#d3c6aa";
+        cursor-color = "#d3c6aa";
+        selection-background = "#475258";
+        selection-foreground = "#d3c6aa";
 
-      palette = 0=#2d353b
-      palette = 1=#e67e80
-      palette = 2=#a7c080
-      palette = 3=#dbbc7f
-      palette = 4=#7fbbb3
-      palette = 5=#d699b6
-      palette = 6=#83c092
-      palette = 7=#d3c6aa
-      palette = 8=#859289
-      palette = 9=#e67e80
-      palette = 10=#a7c080
-      palette = 11=#dbbc7f
-      palette = 12=#7fbbb3
-      palette = 13=#d699b6
-      palette = 14=#83c092
-      palette = 15=#fdf6e3
+        palette = [
+          "0=#2d353b"
+          "1=#e67e80"
+          "2=#a7c080"
+          "3=#dbbc7f"
+          "4=#7fbbb3"
+          "5=#d699b6"
+          "6=#83c092"
+          "7=#d3c6aa"
+          "8=#859289"
+          "9=#e67e80"
+          "10=#a7c080"
+          "11=#dbbc7f"
+          "12=#7fbbb3"
+          "13=#d699b6"
+          "14=#83c092"
+          "15=#fdf6e3"
+        ];
 
-      keybind = clear
-      keybind = ctrl+shift+c=copy_to_clipboard
-      keybind = ctrl+shift+v=paste_from_clipboard
-      keybind = ctrl+0=reset_font_size
-      keybind = ctrl++=increase_font_size:1
-      keybind = ctrl+-=decrease_font_size:1
-    '';
+        keybind = [
+          "clear"
+          "ctrl+shift+c=copy_to_clipboard"
+          "ctrl+shift+v=paste_from_clipboard"
+          "ctrl+0=reset_font_size"
+          "ctrl++=increase_font_size:1"
+          "ctrl+-=decrease_font_size:1"
+        ];
+      };
   };
 }
