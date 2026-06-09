405632cfb17b60fc57625d14988714ddd8057b03
Noah765 <noah.landgraf@gmx.de>
2026-06-09 12:40:19 +0200

    wrappers: refactor overlay definitions

--- modules/wrappers.nix
+++ modules/wrappers.nix
@@ -10,25 +10,26 @@
 
   nixos.imports = lib.mapAttrsToList (_: v: v.install) self.wrappers;
 
-  nixos.config.nixpkgs.overlays = lib.singleton (_: prev: {
-    bat = self.wrappers.bat.wrap {pkgs = prev;};
-    delta = self.wrappers.delta.wrap {pkgs = prev;};
-    desktop-shell = self.wrappers.desktopShell.wrap {pkgs = prev;};
-    eza-wrapped = self.wrappers.eza.wrap {pkgs = prev;};
-    fd = self.wrappers.fd.wrap {pkgs = prev;};
-    fzf = self.wrappers.fzf.wrap {pkgs = prev;};
-    ghostty = self.wrappers.terminal.wrap {pkgs = prev;};
-    git-wrapped = self.wrappers.git.wrap {pkgs = prev;};
-    helix = self.wrappers.editor.wrap {pkgs = prev;};
-    jjui = self.wrappers.vcsTui.wrap {pkgs = prev;};
-    jujutsu = self.wrappers.vcs.wrap {pkgs = prev;};
-    nushell = self.wrappers.shell.wrap {pkgs = prev;};
-    qutebrowser = self.wrappers.browser.wrap {pkgs = prev;};
-    ripgrep-wrapped = self.wrappers.rg.wrap {pkgs = prev;};
-    xdg-desktop-portal-termfilechooser = self.wrappers.termfilechooser.wrap {pkgs = prev;};
-    yazi = self.wrappers.fileManager.wrap {pkgs = prev;};
-    zellij = self.wrappers.multiplexer.wrap {pkgs = prev;};
-    zellij-sessionizer = self.wrappers.multiplexerSessionizer.wrap {pkgs = prev;};
-    zoxide = self.wrappers.cd.wrap {pkgs = prev;};
-  });
+  nixos.config.nixpkgs.overlays = lib.singleton (_: prev:
+    lib.mapAttrs (_: v: self.wrappers.${v}.wrap {pkgs = prev;}) {
+      bat = "bat";
+      delta = "delta";
+      desktop-shell = "desktopShell";
+      eza-wrapped = "eza";
+      fd = "fd";
+      fzf = "fzf";
+      ghostty = "terminal";
+      git-wrapped = "git";
+      helix = "editor";
+      jjui = "vcsTui";
+      jujutsu = "vcs";
+      nushell = "shell";
+      qutebrowser = "browser";
+      ripgrep-wrapped = "rg";
+      xdg-desktop-portal-termfilechooser = "termfilechooser";
+      yazi = "fileManager";
+      zellij = "multiplexer";
+      zellij-sessionizer = "multiplexerSessionizer";
+      zoxide = "cd";
+    });
 }
