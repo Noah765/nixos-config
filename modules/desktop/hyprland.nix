7db6dbe42ef661aeac313ae235ff6ac9c06a2cfb
Noah765 <noah.landgraf@gmx.de>
2026-07-11 11:01:09 +0200

    desktop/hyprland: define overlay without pkgs

--- modules/desktop/hyprland.nix
+++ modules/desktop/hyprland.nix
@@ -15,7 +15,7 @@
       nix.settings.substituters = ["https://hyprland.cachix.org"];
       nix.settings.trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
 
-      nixpkgs.overlays = [(_: _: {inherit (inputs.hyprland.packages.${pkgs.stdenv.system}) xdg-desktop-portal-hyprland;})];
+      nixpkgs.overlays = [(final: _: {inherit (inputs.hyprland.packages.${final.stdenv.system}) xdg-desktop-portal-hyprland;})];
 
       programs.hyprland.enable = true;
       programs.hyprland.withUWSM = true;
