9ae0c1666446e5b9c2ab68e6a3aa0887e2f1c6de
Noah765 <noah.landgraf@gmx.de>
2026-07-21 20:11:12 +0200

    cli/git: configure user.name and user.email

--- modules/cli/git.nix
+++ modules/cli/git.nix
@@ -2,8 +2,10 @@
   nixos.imports = [(lib.mkAliasOptionModule ["cli" "git" "enable"] ["wrappers" "git" "enable"])];
 
   flake.wrappers.git.imports = [lib.w.wrapperModules.git];
-  flake.wrappers.git.settings.credential = {
-    "https://github.com".helper = "!gh auth git-credential";
-    "https://gist.github.com".helper = "!gh auth git-credential";
+  flake.wrappers.git.settings = {
+    user.name = "Noah765";
+    user.email = "noah.landgraf@gmx.de";
+    credential."https://github.com".helper = "!gh auth git-credential";
+    credential."https://gist.github.com".helper = "!gh auth git-credential";
   };
 }
