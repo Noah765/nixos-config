c32b0d466fba2a4da13ef4570efef2c9f1fdde15
Noah765 <noah.landgraf@gmx.de>
2026-07-23 13:32:11 +0200

    cli/pass: enable required gpg-agent

--- modules/cli/pass/default.nix
+++ modules/cli/pass/default.nix
@@ -1,24 +1,38 @@
 {lib, ...}: {
-  nixos = {config, ...}: {
+  nixos = {
+    pkgs,
+    config,
+    ...
+  }: {
     options.cli.pass.enable = lib.mkEnableOption "the password manager pass";
 
     config = lib.mkIf config.cli.pass.enable {
-      hm.programs.password-store.enable = true;
+      hm.programs = {
+        password-store.enable = true;
 
-      # See https://github.com/drduh/YubiKey-Guide
-      hm.programs.gpg = {
+        # See https://github.com/drduh/YubiKey-Guide
+        gpg = {
+          enable = true;
+          mutableKeys = false;
+          mutableTrust = false;
+          publicKeys = lib.singleton {
+            source = ./public-key.asc;
+            trust = "ultimate";
+          };
+          settings = {
+            no-greeting = true;
+            require-secmem = true;
+            throw-keyids = true;
+          };
+        };
+      };
+
+      hm.services.gpg-agent = {
         enable = true;
-        mutableKeys = false;
-        mutableTrust = false;
-        publicKeys = lib.singleton {
-          source = ./public-key.asc;
-          trust = "ultimate";
-        };
-        settings = {
-          no-greeting = true;
-          require-secmem = true;
-          throw-keyids = true;
-        };
+        defaultCacheTtl = 60;
+        maxCacheTtl = 60;
+        noAllowExternalCache = true;
+        pinentry.package = pkgs.pinentry-curses;
       };
 
       core.impermanence.hm.directories = [".password-store"];
