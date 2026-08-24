e8e444589512879c9b5abbb78529d8db4c9b3ce5
Noah765 <noah.landgraf@gmx.de>
2026-08-24 21:33:55 +0200

    cli/devenv: cache passwords

--- modules/cli/devenv.nix
+++ modules/cli/devenv.nix
@@ -8,7 +8,7 @@
 
     config = lib.mkIf config.cli.devenv.enable {
       wrappers.devenv.enable = true;
-      core.impermanence.hm.directories = [".local/share/devenv"];
+      core.impermanence.hm.directories = [".local/share/devenv" ".local/share/secretspec/cache"];
       core.cleanup.script = ''
         cd /home/noah
         ${lib.getExe pkgs.devenv} gc
@@ -33,8 +33,15 @@
       relPath = "${config.binName}-config/secretspec/config.toml";
       builder = ''${lib.getExe' pkgs.remarshal "json2toml"} "$1" "$2"'';
       content = lib.toJSON {
-        defaults.provider = "pass";
-        defaults.profile = "development";
+        defaults = {
+          provider = "cached-pass";
+          profile = "development";
+          providers.cached-pass.uri = "pass";
+          providers.cached-pass.cache = {
+            provider = "file:~/.local/state/secretspec/cache";
+            max_age = "1w";
+          };
+        };
         audit.enable = false;
       };
     };
